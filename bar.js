faaeca47f2183913f4d660ad3165ac288c23a0e7
Julia Janicki <jhjanicki@gmail.com>
2017-07-20 13:29:36 +0900

    first

---
+++ bar.js
@@ -0,0 +1,145 @@
+d3.custom = {};
+d3.custom.barChart = function module(){
+
+	var margin = {top:20, right:20, bottom:120, left:80};
+	var width = 500;
+	var height = 300;
+	var gap = 0;
+	var _index = 0;
+	
+	var svg;
+	
+	var year = '';
+
+	
+	
+	
+	
+	function exports(_selection) {
+        _selection.each(function(_data) {
+        
+        	console.log(_data);
+        
+        	var chartW = width - margin.left - margin.right,
+                chartH = height - margin.top - margin.bottom;
+        
+        	var x = d3.scale.ordinal()
+        		.domain(_data.map(function(d) { return d.Country; }))
+        		.rangeRoundBands([0, chartW], .4);
+			var y = d3.scale.linear()
+			    .domain((d3.extent(_data, function(d) { return parseInt(d[year]); })))
+			    .nice()
+			    .range([chartH,0]);
+			    
+			console.log(y.domain());
+			console.log(year);
+			    
+			var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom");
+
+            var yAxis = d3.svg.axis()
+                    .scale(y)
+                    .ticks(8)
+                    .orient("left");
+            
+             var barW = width / _data.length;
+             
+             if (!svg) {
+                svg = d3.select(this)
+                    .append("svg")
+                    .classed("chart", true);
+                var container = svg.append("g").classed("container-group", true);
+                container.append("g").classed("chart-group", true);
+                container.append("g").classed("x-axis-group axis", true);
+                container.append("g").classed("y-axis-group axis", true);
+            }
+        	
+        	
+        	svg.transition().attr({width: width, height: height});
+            svg.select(".container-group")
+                .attr({transform: "translate(" + margin.left + "," + margin.top + ")"});
+
+            svg.select(".x-axis-group.axis")
+                .attr({transform: "translate(0," + (chartH) + ")"})
+                .call(xAxis)
+                .selectAll("text")	
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-65)" 
+                });
+
+
+            svg.select(".y-axis-group.axis")
+                .transition()
+                .call(yAxis);
+            
+            var gapSize = x.rangeBand() / 100 * gap;
+            var barW = x.rangeBand() - gapSize;
+			
+
+            var bars = svg.select(".chart-group")
+                    .selectAll(".bar")
+                    .data(_data);
+                    
+            bars.enter().append("rect")
+                .classed("bar", true)
+                .attr("x", function(d) { return x(d.Country); })
+      			.attr("y", function(d) { return y(d[year]); })
+      			.attr("width", barW)
+      			.attr("height", function(d) { return chartH - y(d[year]); })
+      			.attr("fill","#2166ac");
+               
+        	
+        	bars.transition()
+        	 .attr("x", function(d) { return x(d.Country); })
+      			.attr("y", function(d) { return y(d[year]); })
+      			.attr("width", barW)
+      			.attr("height", function(d) { return chartH - y(d[year]); });
+
+        	
+            bars.exit().transition().style({opacity: 0}).remove();
+        
+        });
+        
+    }
+    
+    exports.width = function(_x) {
+        if (!arguments.length) return width;
+        width = parseInt(_x);
+        return this;
+    };
+    exports.height = function(_x) {
+        if (!arguments.length) return height;
+        height = parseInt(_x);
+        return this;
+    };
+    
+    exports._index = function(_x) {
+        if (!arguments.length) return _index;
+        _index = parseInt(_x);
+        return this;
+    };
+    
+    exports.year = function(_x) {
+        if (!arguments.length) return year;
+        year = parseInt(_x);
+        return this;
+    };
+
+    return exports;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data/not_using/importData.js
@@ -0,0 +1,122 @@
+var importData = [
+  {
+    "Year": 2000,
+    "All": 40000,
+    "LatinAmerica": 180,
+    "Africa": 4000,
+    "Asia": 35800,
+    "Other": 20
+  },
+  {
+    "Year": 2001,
+    "All": 35000,
+    "LatinAmerica": 260,
+    "Africa": 6000,
+    "Asia": 28700,
+    "Other": 40
+  },
+  {
+    "Year": 2002,
+    "All": 30000,
+    "LatinAmerica": 350,
+    "Africa": 14800,
+    "Asia": 14800,
+    "Other": 50
+  },
+  {
+    "Year": 2003,
+    "All": 30000,
+    "LatinAmerica": 180,
+    "Africa": 6000,
+    "Asia": 23800,
+    "Other": 20
+  },
+  {
+    "Year": 2004,
+    "All": 32000,
+    "LatinAmerica": 280,
+    "Africa": 3000,
+    "Asia": 28700,
+    "Other": 20
+  },
+  {
+    "Year": 2005,
+    "All": 42000,
+    "LatinAmerica": 460,
+    "Africa": 6000,
+    "Asia": 35500,
+    "Other": 40
+  },
+  {
+    "Year": 2006,
+    "All": 39000,
+    "LatinAmerica": 450,
+    "Africa": 4000,
+    "Asia": 34500,
+    "Other": 50
+  },
+  {
+    "Year": 2007,
+    "All": 100000,
+    "LatinAmerica": 900,
+    "Africa": 9000,
+    "Asia": 90000,
+    "Other": 100
+  },
+  {
+    "Year": 2008,
+    "All": 70000,
+    "LatinAmerica": 900,
+    "Africa": 32000,
+    "Asia": 32000,
+    "Other": 100
+  },
+  {
+    "Year": 2009,
+    "All": 50000,
+    "LatinAmerica": 1800,
+    "Africa": 13000,
+    "Asia": 35000,
+    "Other": 200
+  },
+  {
+    "Year": 2010,
+    "All": 150000,
+    "LatinAmerica": 9000,
+    "Africa": 70000,
+    "Asia": 70000,
+    "Other": 1000
+  },
+  {
+    "Year": 2011,
+    "All": 550000,
+    "LatinAmerica": 80000,
+    "Africa": 200000,
+    "Asia": 250000,
+    "Other": 20000
+  },
+  {
+    "Year": 2012,
+    "All": 800000,
+    "LatinAmerica": 170000,
+    "Africa": 370000,
+    "Asia": 250000,
+    "Other": 10000
+  },
+  {
+    "Year": 2013,
+    "All": 1100000,
+    "LatinAmerica": 150000,
+    "Africa": 480000,
+    "Asia": 420000,
+    "Other": 50000
+  },
+  {
+    "Year": 2014,
+    "All": 1950000,
+    "LatinAmerica": 100000,
+    "Africa": 900000,
+    "Asia": 900000,
+    "Other": 50000
+  }
+];	
